4f310dc293de0134309b247739cd92366c7c4d60
Boryana Dimitrova <b.dimitrova97@gmail.com>
2024-10-16 15:38:14 +0300

    Convert and separate
    Convert HTML to JSX and separate HTML into components.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import Header from "./components/header/Header";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return <></>;
+    return (
+        <>
+            <div id="box">
+                <main id="main-content">
+                    <Header />
+                </main>
+            </div>
+        </>
+    );
 }
 
 export default App;
