3c1d50a87034d848cfc4b0953fb4eb5dbbfbe086
Boryana Dimitrova <b.dimitrova97@gmail.com>
2024-10-16 18:26:45 +0300

    Implement no games message

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -5,15 +5,19 @@ import GameListItem from "./game-list-item/GameListItem";
 export default function Catalog() {
     const [games, setGames] = useState([]);
     useEffect(() => {
-        gamesAPI.getAll().then((result) => {setGames(result)});
+        gamesAPI.getAll().then((result) => {
+            setGames(result);
+        });
     }, []);
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            {games.map(game => <GameListItem key={game._id} {...game} />)}
-
-            <h3 className="no-articles">No articles yet</h3>
+            {games.length > 0 ? (
+                games.map((game) => <GameListItem key={game._id} {...game} />)
+            ) : (
+                <h3 className="no-articles">No articles yet</h3>
+            )}
         </section>
     );
 }
